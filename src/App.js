8bdd0beb4a4bf2850991ca1e6a462732d0b4b3f0
Lloydinator <lloydmiller@outlook.com>
2020-08-29 00:33:21 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <h1 className="text-lg font-bold">Hello, this is a <span className="text-red-500">MOTHERFUCKING WARNING</span></h1>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import 'typeface-montserrat'
+
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      'display': ['Montserrat', 'sans-serif'],
+      'body': ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
